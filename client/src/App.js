4b2545cf818116342986c8b36d6caba33ddcdb1b
Adan <jonesadan@yahoo.com>
2024-04-30 23:21:56 -0400

    changed URL to match live web service monorepo

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ const App = () => {
   const [name, setName] = useState([])
 
   useEffect(() => {
-    fetch('https://starter-name-generator.onrender.com/generate') // Change this URL to the one that you just deployed
+    fetch('https://starter-name-generator-23xe.onrender.com/generate') // Change this URL to the one that you just deployed
       .then((res) => res.json())
       .then((name) => setName([name]))
   }, [])
